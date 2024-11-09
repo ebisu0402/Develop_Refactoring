eecd1e87486da3704454f04203ef19be25bcfffa
ebisu0402 <oca220424003@edu.oca.ac.jp>
2024-11-09 15:08:08 +0900

    summary記入後

--- check_and_repeat.py
+++ check_and_repeat.py
@@ -1,4 +1,17 @@
 def check_string(text, repeat_horizontal, repeat_vertical):
+    """_summary_
+
+    Args:
+        text (str): 繰り返し対象の文字列
+        repeat_horizontal (int):文字列を横に繰り返す回数。
+        repeat_vertical (int): 文字列を縦に繰り返す回数
+    Raises:
+        ValueError: 文字列が5文字以上、１０文字以下
+        ValueError: 文字列はアルファベットのみ
+
+    Returns:
+          str: 指定された回数で繰り返された文字列。
+    """
     # 1. 文字列の長さチェック
     if len(text) < 5 or len(text) > 10:
         raise ValueError("文字列は5文字以上、10文字以下でなければなりません。")
